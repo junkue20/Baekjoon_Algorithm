b10abadafed2a25e0e7683d8e8f1c4a9bb5c2886
Jungyu Park <122848687+junkue20@users.noreply.github.com>
2024-02-13 14:48:06 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 120 ms, Memory: 9348 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.js
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.js
@@ -22,17 +22,7 @@ const [N, ...num] = input;
 // answer(num);
 
 const answer = (num) => {
-  const dp = new Array(11);
-  dp[1] = 1;
-  dp[2] = 2;
-  dp[3] = 4;
-  dp[4] = 7;
-  dp[5] = 13;
-  dp[6] = 24;
-  dp[7] = 44;
-  dp[8] = 81;
-  dp[9] = 149;
-  dp[10] = 274;
+  const dp = [, 1, 2, 4, 7, 13, 24, 44, 81, 149, 274];
 
   for (let i = 0; i < N; i++) {
     console.log(dp[Number(num[i])]);
