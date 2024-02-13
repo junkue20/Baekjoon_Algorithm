141babfeae4c07618e4c0ae46edeb4e10431e3c5
Jungyu Park <122848687+junkue20@users.noreply.github.com>
2024-02-13 22:17:32 +0900

    9095 문제 제출 수정

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.js
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.js
@@ -2,27 +2,16 @@ const filePath = process.platform === 'linux' ? 'dev/stdin' : './input.txt';
 const input = require('fs').readFileSync(filePath).toString().trim().split('\n');
 const [N, ...num] = input;
 
-// 그냥 1, 2, 3도 포함이 되는구나;; 점화식 찾는데 늦어버림..
-
-// const answer = (num) => {
-//   const dp = new Array(11);
-//   dp[1] = 1;
-//   dp[2] = 2;
-//   dp[3] = 4;
-
-//   for (let i = 4; i < dp.length; i++) {
-//     dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
-//   }
-
-//   for (let i = 0; i < N; i++) {
-//     console.log(dp[Number(num[i])]);
-//   }
-// };
-
-// answer(num);
-
+// 그냥 1, 2, 3도 포함이 되는구나.. 점화식 찾는데 시간이 조금 걸렸음.
 const answer = (num) => {
-  const dp = [, 1, 2, 4, 7, 13, 24, 44, 81, 149, 274];
+  const dp = new Array(11);
+  dp[1] = 1;
+  dp[2] = 2;
+  dp[3] = 4;
+
+  for (let i = 4; i < dp.length; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+  }
 
   for (let i = 0; i < N; i++) {
     console.log(dp[Number(num[i])]);
@@ -30,3 +19,5 @@ const answer = (num) => {
 };
 
 answer(num);
+
+
