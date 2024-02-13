edb0fe62f4f595a275121a5571697f2c424a4cab
Jungyu Park <122848687+junkue20@users.noreply.github.com>
2024-02-13 14:47:13 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 124 ms, Memory: 9360 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.js
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.js
@@ -4,15 +4,35 @@ const [N, ...num] = input;
 
 // 그냥 1, 2, 3도 포함이 되는구나;; 점화식 찾는데 늦어버림..
 
+// const answer = (num) => {
+//   const dp = new Array(11);
+//   dp[1] = 1;
+//   dp[2] = 2;
+//   dp[3] = 4;
+
+//   for (let i = 4; i < dp.length; i++) {
+//     dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+//   }
+
+//   for (let i = 0; i < N; i++) {
+//     console.log(dp[Number(num[i])]);
+//   }
+// };
+
+// answer(num);
+
 const answer = (num) => {
   const dp = new Array(11);
   dp[1] = 1;
   dp[2] = 2;
   dp[3] = 4;
-
-  for (let i = 4; i < dp.length; i++) {
-    dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
-  }
+  dp[4] = 7;
+  dp[5] = 13;
+  dp[6] = 24;
+  dp[7] = 44;
+  dp[8] = 81;
+  dp[9] = 149;
+  dp[10] = 274;
 
   for (let i = 0; i < N; i++) {
     console.log(dp[Number(num[i])]);
