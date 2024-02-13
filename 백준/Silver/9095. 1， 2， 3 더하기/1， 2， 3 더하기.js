22892f77bc9818a2e5ba04078ab3746dc5d04a0c
Jungyu Park <122848687+junkue20@users.noreply.github.com>
2024-02-13 14:44:29 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 124 ms, Memory: 9364 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.js
@@ -0,0 +1,22 @@
+const filePath = process.platform === 'linux' ? 'dev/stdin' : './input.txt';
+const input = require('fs').readFileSync(filePath).toString().trim().split('\n');
+const [N, ...num] = input;
+
+// 그냥 1, 2, 3도 포함이 되는구나;; 점화식 찾는데 늦어버림..
+
+const answer = (num) => {
+  const dp = new Array(11);
+  dp[1] = 1;
+  dp[2] = 2;
+  dp[3] = 4;
+
+  for (let i = 4; i < dp.length; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+  }
+
+  for (let i = 0; i < N; i++) {
+    console.log(dp[Number(num[i])]);
+  }
+};
+
+answer(num);
